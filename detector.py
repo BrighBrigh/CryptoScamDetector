b1ca9437ea9582660ebb223d1d065da9eca00d8d
Brian Winkler <brianewinkler@gmail.com>
2018-04-19 12:11:23 -0700

    Update detector.py

--- detector.py
+++ detector.py
@@ -9,20 +9,34 @@ ACCESS_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 status_reply = "WARNING! This post has been made by an impostor attempting to run a scam! Please continue accordingly. #CryptoScamDetector"
 
 account_list = ['vergecurrency']
+blacklist = []
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    IMPOSTOR = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def updateScammerStatus(name):
-	post = "@"+name+" has been detected to be an impostor!"
-	update = api.update_status()
+	post = "@"+name+" has been detected to be an impostor! #CryptoScamDetector"
+	update = api.update_status(status=post)
 
 def reportSpam(id):
 	report = api.report_spam(user_id=id)
-	print(id, " has been reported!")
+	print(id,"has been reported!")
 
+def replyPost(id, name):
+	status= "@"+name+" WARNING! This post has been detected to be a scam and has been thusly reported. #CryptoScamAlert"
+	reply = api.update_status(status,id)
 
 
 def getUserID(accounts):
@@ -48,32 +62,44 @@ def getReplies(name):
 	post_tag_name = json.loads(tag_parsed)
 	post_name = post_tag_name['name']
 	for full_tweets in tweepy.Cursor(api.user_timeline,screen_name=name,timeout=999999).items(10):
-		for tweet in tweepy.Cursor(api.search,q='to:'+name,result_type='recent',timeout=999999).items(200):
+		print("Fetching Replies")
+		for tweet in tweepy.Cursor(api.search,q='to:'+name,result_type='recent',timeout=999999).items(100):
 			if hasattr(tweet, 'in_reply_to_status_id_str'):
 				if(tweet.in_reply_to_status_id_str==full_tweets.id_str):
 					replies.append(tweet.id)
 		for element in replies:
+			blacklist_flag = False
 			stat = api.get_status(id=element)
 			parsed = json.dumps(stat._json)
 			parsed = json.loads(parsed)
 			screen_name = parsed['user']['screen_name']
 			user_name = parsed['user']['name']
-			if(user_name == post_name):
-				if(screen_name != name):
-					print("Impostor!")
-					print(screen_name)
-					print(user_name)
-					id = parsed['user']['id']
-					print(parsed['user']['id'])
-					reportSpam(id=id)
-					status = api.update_status(status_reply, in_reply_to_status_id=element)
-					updateScammerStatus(name=user_name)
+			for b in blacklist:
+				if(b == element):
+					blacklist_flag = True
+			if(blacklist_flag == False):
+				if(user_name == post_name):
+					if(screen_name != name):
+						print(bcolors.IMPOSTOR + "Impostor detected!" + bcolors.ENDC)
+						print(screen_name, user_name)
+						id = parsed['user']['id']
+						replyPost(id=element, name=screen_name)
+						reportSpam(id=id)
+						f = open("imposter_list.txt", "a+")
+						f.write("Username: %s\n" % user_name)
+						f.write("Screen name: %s\n" % screen_name)
+						f.write("User id: %s\n" % id)
+						f.write("Offending Tweet Id: %d\n\n" % element)
+						f.close()
+						blacklist.append(element)
+
 		replies.clear()
 
-id_list = getUserID(accounts = account_list)
-tweet_id = getTweetId(accounts = account_list)
 
 for account in account_list:
+	print(bcolors.OKGREEN + "Running check for " + account + bcolors.ENDC)
 	getReplies(name=account)
-	print("Sleeping...")
-	time.sleep(400)
+	print("Sleeping for 15 minutes...")
+	time.sleep(900)
+
+
